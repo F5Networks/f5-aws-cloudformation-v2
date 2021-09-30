dc4e5488bf8695ac2f2852eb306daffa61870782
shimkus <m.shimkus@f5.com>
2021-09-30 00:12:40 +0000

    Release commited to v1.1.0.0 tag

--- automated-test-scripts/f5-aws-cloudformation-v2/common/capture_stack_events.sh
+++
@@ -1,9 +0,0 @@
-#  expectValue = "Stack Events"
-#  scriptTimeout = 2
-#  replayEnabled = false
-#  replayTimeout = 10
-
-
-echo "-------------------------------Stack Events---------------------------------"
-aws cloudformation describe-stack-events --region <REGION> --stack-name <STACK NAME>|jq '.StackEvents[]'
-echo "----------------------------End Stack Events--------------------------------"
--- automated-test-scripts/f5-aws-cloudformation-v2/examples/modules/bigip-standalone/tests/verify_bigip_instance.sh
+++
@@ -1,9 +0,0 @@
-#  expectFailValue = "FAIL"
-#  scriptTimeout = 3
-#  replayEnabled = false
-
-instance_id=$(aws cloudformation describe-stacks --stack-name <STACK NAME> --region <REGION> | jq -r '.Stacks[].Outputs[] | select (.OutputKey=="bigIpInstanceId") | .OutputValue')
-bigip_instance_metadata=$(aws ec2 describe-instances --region <REGION> --instance-ids $instance_id | jq -r .Reservations[0].Instances[0])
-
-[[ "<PUBLIC IP>" == "Yes" ]] && [[ -z $(echo $bigip_instance_metadata | jq .PublicIpAddress) ]] && echo "FAIL"
-[[ $(echo $bigip_instance_metadata | jq .NetworkInterfaces | jq length) != "<NUMBER SUBNETS>" ]] && echo "FAIL"
