d8ae0b234507711a4f690d7bebe8d033cdcf428f
shimkus <m.shimkus@f5.com>
2022-05-11 20:47:48 +0000

    Release commited to v2.1.0.0 tag

--- automated-test-scripts/f5-aws-cloudformation-v2/common/delete_access_stack.sh
+++
@@ -1,7 +0,0 @@
-#  expectValue = "PASS"
-#  scriptTimeout = 10
-#  replayEnabled = false
-#  replayTimeout = 0
-
-aws cloudformation delete-stack --region <REGION> --stack-name <ACCESS STACK NAME>
-echo "PASS"
