8aaf5d213f174eb6809e6832022746b34a55f9a1
Michael Shimkus <m.shimkus@f5.com>
2022-06-16 21:57:34 +0000

    ESECLDTPLT-3063 - Functional test improvements

--- automated-test-scripts/f5-aws-cloudformation-v2/examples/failover/payg_byol/create_stack_defaults.sh
+++ automated-test-scripts/f5-aws-cloudformation-v2/examples/failover/payg_byol/create_stack_defaults.sh
@@ -12,8 +12,6 @@ curl <TEMPLATE URL> -o <DEWPOINT JOB ID>-template.yaml
 artifact_location=$(cat <DEWPOINT JOB ID>-template.yaml | yq -r .Parameters.artifactLocation.Default)
 echo "artifact_location=$artifact_location"
 
-secret_name=$(aws secretsmanager describe-secret --secret-id <DEWPOINT JOB ID>-secret-runtime --region <REGION> | jq -r .Name)
-secret_arn=$(aws secretsmanager describe-secret --secret-id <DEWPOINT JOB ID>-secret-runtime --region <REGION> | jq -r .ARN)
 region=$(aws s3api get-bucket-location --bucket $bucket_name | jq -r .LocationConstraint)
 
 if [ -z $region ] || [ $region == null ]; then
@@ -21,54 +19,46 @@ if [ -z $region ] || [ $region == null ]; then
     echo "using default bucket region:$region"
 fi
 
-runtimeConfig01='"<RUNTIME INIT CONFIG 01>"'
-runtimeConfig02='"<RUNTIME INIT CONFIG 02>"'
-
-if [[ "<PROVISION EXAMPLE APP>" == "false" ]]; then
-    declare -a runtime_init_config_files=(/$PWD/examples/failover/bigip-configurations/runtime-init-conf-3nic-payg-instance01.yaml /$PWD/examples/failover/bigip-configurations/runtime-init-conf-3nic-payg-instance02.yaml)
-else
-    declare -a runtime_init_config_files=(/$PWD/examples/failover/bigip-configurations/runtime-init-conf-3nic-payg-instance01-with-app.yaml /$PWD/examples/failover/bigip-configurations/runtime-init-conf-3nic-payg-instance02-with-app.yaml)
+if echo "<TEMPLATE URL>" | grep -q "existing-network"; then
+    echo "Setting existing stack variables"
+    mgmtAz1=$(aws cloudformation describe-stacks --region <REGION> --stack-name <NETWORK STACK NAME> | jq  -r '.Stacks[0].Outputs[] | select(.OutputKey=="subnetsA").OutputValue' | cut -d ',' -f 2)
+    extAz1=$(aws cloudformation describe-stacks --region <REGION> --stack-name <NETWORK STACK NAME> | jq  -r '.Stacks[0].Outputs[] | select(.OutputKey=="subnetsA").OutputValue' | cut -d ',' -f 1)
+    intAz1=$(aws cloudformation describe-stacks --region <REGION> --stack-name <NETWORK STACK NAME> | jq  -r '.Stacks[0].Outputs[] | select(.OutputKey=="subnetsA").OutputValue' | cut -d ',' -f 3)
+    mgmtAz2=$(aws cloudformation describe-stacks --region <REGION> --stack-name <NETWORK STACK NAME> | jq  -r '.Stacks[0].Outputs[] | select(.OutputKey=="subnetsB").OutputValue' | cut -d ',' -f 2)
+    extAz2=$(aws cloudformation describe-stacks --region <REGION> --stack-name <NETWORK STACK NAME> | jq  -r '.Stacks[0].Outputs[] | select(.OutputKey=="subnetsB").OutputValue' | cut -d ',' -f 1)
+    intAz2=$(aws cloudformation describe-stacks --region <REGION> --stack-name <NETWORK STACK NAME> | jq  -r '.Stacks[0].Outputs[] | select(.OutputKey=="subnetsB").OutputValue' | cut -d ',' -f 3)
+    vpcId=$(aws cloudformation describe-stacks --region <REGION> --stack-name <NETWORK STACK NAME> | jq  -r '.Stacks[0].Outputs[] | select(.OutputKey=="vpcId").OutputValue')
 fi
-counter=1
-for config_name in "${runtime_init_config_files[@]}"; do
 
-    # Download and modify runtime-init, then upload to s3.
-    curl https://f5-cft-v2.s3.amazonaws.com/${artifact_location}failover/bigip-configurations/$config_name -o <DEWPOINT JOB ID>-config.yaml
+# Here we use a static secret that matches the default in the runtime init config
+# The secret is replicated to the US regions and Canada central
+secret_arn="arn:aws:secretsmanager:<REGION>:089591600128:secret:mySecretId-n0PHSR"
 
-    # Create user for tests that connect to the REST API
-    /usr/bin/yq e ".extension_services.service_operations.[0].value.Common.admin.class = \"User\"" -i <DEWPOINT JOB ID>-config.yaml
-    /usr/bin/yq e ".extension_services.service_operations.[0].value.Common.admin.password = \"{{{BIGIP_PASSWORD}}}\"" -i <DEWPOINT JOB ID>-config.yaml
-    /usr/bin/yq e ".extension_services.service_operations.[0].value.Common.admin.shell = \"bash\"" -i <DEWPOINT JOB ID>-config.yaml
-    /usr/bin/yq e ".extension_services.service_operations.[0].value.Common.admin.userType = \"regular\"" -i <DEWPOINT JOB ID>-config.yaml
+# upload to s3
+aws s3 cp --region <REGION> <DEWPOINT JOB ID>-template.yaml s3://"$bucket_name"/<DEWPOINT JOB ID>-template.yaml --acl public-read
 
-    # Disable AutoPhoneHome
-    /usr/bin/yq e ".extension_services.service_operations.[0].value.Common.My_System.autoPhonehome = false" -i <DEWPOINT JOB ID>-0$counter.yaml
+# create parameters
+parameters="ParameterKey=restrictedSrcAddressMgmt,ParameterValue=$src_ip \
+ParameterKey=restrictedSrcAddressApp,ParameterValue=$src_ip \
+ParameterKey=secretArn,ParameterValue=$secret_arn \
+ParameterKey=cfeS3Bucket,ParameterValue=bigip-ha-solution-<DEWPOINT JOB ID> \
+ParameterKey=uniqueString,ParameterValue=<UNIQUESTRING> \
+ParameterKey=sshKey,ParameterValue=<SSH KEY>"
 
-    # Runtime parameters
-    /usr/bin/yq e ".runtime_parameters += {\"name\":\"BIGIP_PASSWORD\"}" -i <DEWPOINT JOB ID>-0$counter.yaml
-    /usr/bin/yq e ".runtime_parameters.[4].type = \"secret\"" -i <DEWPOINT JOB ID>-0$counter.yaml
-    /usr/bin/yq e ".runtime_parameters.[4].secretProvider.environment = \"aws\"" -i <DEWPOINT JOB ID>-0$counter.yaml
-    /usr/bin/yq e ".runtime_parameters.[4].secretProvider.secretId = \"$secret_name\"" -i <DEWPOINT JOB ID>-0$counter.yaml
-    /usr/bin/yq e ".runtime_parameters.[4].secretProvider.type = \"SecretsManager\"" -i <DEWPOINT JOB ID>-0$counter.yaml
-    /usr/bin/yq e ".runtime_parameters.[4].secretProvider.version = \"AWSCURRENT\"" -i <DEWPOINT JOB ID>-0$counter.yaml
+if echo "<TEMPLATE URL>" | grep -q "existing-network"; then
+    echo "Adding existing stack parameters"
+    parameters+=" ParameterKey=bigIpMgmtSubnetId01,ParameterValue=$mgmtAz1 \
+    ParameterKey=bigIpMgmtSubnetId02,ParameterValue=$mgmtAz2 \
+    ParameterKey=bigIpExternalSubnetId01,ParameterValue=$extAz1 \
+    ParameterKey=bigIpExternalSubnetId02,ParameterValue=$extAz2 \
+    ParameterKey=bigIpInternalSubnetId01,ParameterValue=$intAz1 \
+    ParameterKey=bigIpInternalSubnetId02,ParameterValue=$intAz2 \
+    ParameterKey=vpcId,ParameterValue=$vpcId"
+fi
+echo "Parameters:$parameters"
 
-    # print out config file
-    /usr/bin/yq e <DEWPOINT JOB ID>-config.yaml
-
-    # upload to s3
-    aws s3 cp --region <REGION> <DEWPOINT JOB ID>-template.yaml s3://"$bucket_name"/<DEWPOINT JOB ID>-template.yaml --acl public-read
-    aws s3 cp --region <REGION> <DEWPOINT JOB ID>-config.yaml s3://"$bucket_name"/<DEWPOINT JOB ID>-config.yaml --acl public-read
-
-    ((counter=counter+1))
-done
-
-    # create stack
-    aws cloudformation create-stack --disable-rollback --region <REGION> --stack-name <STACK NAME> --tags Key=creator,Value=dewdrop Key=delete,Value=True \
-    --template-url <TEMPLATE S3 URL> \
-    --capabilities CAPABILITY_IAM \
-    --parameters ParameterKey=bigIpRuntimeInitConfig01,ParameterValue=$runtimeConfig01 \
-    ParameterKey=bigIpRuntimeInitConfig02,ParameterValue=$runtimeConfig02 \
-    ParameterKey=restrictedSrcAddressMgmt,ParameterValue=$src_ip \
-    ParameterKey=restrictedSrcAddressApp,ParameterValue=$src_ip \
-    ParameterKey=uniqueString,ParameterValue=<UNIQUESTRING> \
-    ParameterKey=sshKey,ParameterValue=<SSH KEY>
+# create stack
+aws cloudformation create-stack --disable-rollback --region <REGION> --stack-name <STACK NAME> --tags Key=creator,Value=dewdrop Key=delete,Value=True \
+--template-url https://s3.amazonaws.com/"$bucket_name"/<DEWPOINT JOB ID>-template.yaml \
+--capabilities CAPABILITY_IAM \
+--parameters $parameters
