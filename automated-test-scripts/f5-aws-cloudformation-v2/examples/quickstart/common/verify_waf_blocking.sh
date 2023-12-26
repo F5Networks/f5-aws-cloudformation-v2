5ff8c23aa678f843fb2d0cd4d30b1ee9fca2763f
Michael Shimkus <m.shimkus@f5.com>
2023-12-26 08:39:50 -0800

    Resolve EC-392 "Release"

--- automated-test-scripts/f5-aws-cloudformation-v2/examples/quickstart/common/verify_waf_blocking.sh
+++ automated-test-scripts/f5-aws-cloudformation-v2/examples/quickstart/common/verify_waf_blocking.sh
@@ -4,7 +4,7 @@
 #  replayTimeout = 10
 
 
-if [[ "<PROVISION EXAMPLE APP>" == 'false' ]]; then
+if [[ "<PROVISION EXAMPLE APP>" == 'false' || ( "<PROVISION PUBLIC IP>" == 'false' && "<NIC COUNT>" == '1' ) ]]; then
     echo "PASS"
 else
     dnsAppname=$(aws cloudformation describe-stacks --stack-name <STACK NAME> --region <REGION> | jq -r '.Stacks[].Outputs[] | select (.OutputKey=="vipPublicUrl") | .OutputValue')
