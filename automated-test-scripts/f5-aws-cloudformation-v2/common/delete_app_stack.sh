07a63054424ef38082e301ca5da66d928d9f7bdd
shimkus <m.shimkus@f5.com>
2023-12-26 17:45:04 +0000

    Release commited to v3.2.0.0 tag

--- automated-test-scripts/f5-aws-cloudformation-v2/common/delete_app_stack.sh
+++
@@ -1,7 +0,0 @@
-#  expectValue = "PASS"
-#  scriptTimeout = 10
-#  replayEnabled = false
-#  replayTimeout = 0
-
-aws cloudformation delete-stack --region <REGION> --stack-name app-<STACK NAME>
-echo "PASS"
--- automated-test-scripts/f5-aws-cloudformation-v2/common/delete_bastion_stack.sh
+++
@@ -1,7 +0,0 @@
-#  expectValue = "PASS"
-#  scriptTimeout = 10
-#  replayEnabled = false
-#  replayTimeout = 0
-
-aws cloudformation delete-stack --region <REGION> --stack-name bastion-<STACK NAME>
-echo "PASS"
