f5de1c5849604cd8a183eb9960283f2782e8084e
shimkus <m.shimkus@f5.com>
2024-04-17 15:00:06 +0000

    Release commited to v3.4.0.0 tag

--- automated-test-scripts/f5-aws-cloudformation-v2/common/delete_dag_stack.sh
+++
@@ -1,7 +0,0 @@
-#  expectValue = "PASS"
-#  scriptTimeout = 10
-#  replayEnabled = false
-#  replayTimeout = 0
-
-aws cloudformation delete-stack --region <REGION> --stack-name <DAG STACK NAME>
-echo "PASS"
